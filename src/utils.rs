65633e9b41e8d721e7eb3b7ca576ff856fbc50b0
sigoden <sigoden@gmail.com>
2023-03-03 02:41:00 +0800

    fix: hidden don't works on some files
    --hidden '*.abc-cba' matches xyz.abc-cba but do not matches 123.xyz.abc-cba

--- src/utils.rs
+++ src/utils.rs
@@ -34,45 +34,12 @@ pub fn try_get_file_name(path: &Path) -> Result<&str> {
         .ok_or_else(|| anyhow!("Failed to get file name of `{}`", path.display()))
 }
 
-pub fn glob(source: &str, target: &str) -> bool {
-    let ss: Vec<char> = source.chars().collect();
-    let mut iter = target.chars();
-    let mut i = 0;
-    'outer: while i < ss.len() {
-        let s = ss[i];
-        match s {
-            '*' => match ss.get(i + 1) {
-                Some(s_next) => {
-                    for t in iter.by_ref() {
-                        if t == *s_next {
-                            i += 2;
-                            continue 'outer;
-                        }
-                    }
-                    return false;
-                }
-                None => return true,
-            },
-            '?' => match iter.next() {
-                Some(_) => {
-                    i += 1;
-                    continue;
-                }
-                None => return false,
-            },
-            _ => match iter.next() {
-                Some(t) => {
-                    if s == t {
-                        i += 1;
-                        continue;
-                    }
-                    return false;
-                }
-                None => return false,
-            },
-        }
-    }
-    iter.next().is_none()
+pub fn glob(pattern: &str, target: &str) -> bool {
+    let pat = match ::glob::Pattern::new(pattern) {
+        Ok(pat) => pat,
+        Err(_) => return false,
+    };
+    pat.matches(target)
 }
 
 #[test]
@@ -91,6 +58,10 @@ fn test_glob_key() {
     assert!(!glob("abc", "abcd"));
     assert!(!glob("a?c", "abbc"));
     assert!(!glob("*.log", "log"));
+    assert!(glob("*.abc-cba", "xyz.abc-cba"));
+    assert!(glob("*.abc-cba", "123.xyz.abc-cba"));
     assert!(glob("*.log", ".log"));
     assert!(glob("*.log", "a.log"));
+    assert!(glob("*/", "abc/"));
+    assert!(!glob("*/", "abc"));
 }
